7b22477b77985b0c455a06d55e1a145181a4e9c3
Peter Laudel <38426989+PeterLaudel@users.noreply.github.com>
2024-07-03 20:32:05 +0200

    Add create form and create sheet

--- Code.ts
+++ Code.ts
@@ -1,22 +1,74 @@
-function onEdit(e) {
-  var sheet = e.source.getActiveSheet();
-  var editedRow = e.range.getRow();
-  var lastRow = sheet.getLastRow();
+function createEnvironment() {
+  const sheet = createSheet();
+  const form = createForm();
 
-  sheet.getRange(lastRow, 1).setValue("Neuer Wert");
+  form.setDestination(FormApp.DestinationType.SPREADSHEET, sheet.getId());
 }
 
-function onSubmitForm(e: GoogleAppsScript.Events.FormsOnFormSubmit) {
-  var sheet = SpreadsheetApp.getActiveSpreadsheet().getActiveSheet();
-  var lastRow = sheet.getLastRow();
-  var lastColumn = sheet.getLastColumn();
-
-  // Fügt eine neue Spalte nach der letzten Spalte hinzu, wenn sie noch nicht existiert
-  if (sheet.getRange(1, lastColumn).getValue() !== "Zusätzlicher Eintrag") {
-    sheet.insertColumnAfter(lastColumn);
-    sheet.getRange(1, lastColumn + 1).setValue("Zusätzlicher Eintrag");
+function findFileByName(
+  fileName: string,
+  files: GoogleAppsScript.Drive.FileIterator
+) {
+  while (files.hasNext()) {
+    const file = files.next();
+    if (file.getName() === fileName) {
+      return file;
+    }
   }
+  return null;
+}
 
-  // Setzt den Wert der Zelle in der neuen Spalte für die letzte Zeile
-  sheet.getRange(lastRow, lastColumn + 1).setValue("Neuer Wert");
+function createSheet() {
+  const fileName = "Patienten";
+  const files = DriveApp.getFilesByType(MimeType.GOOGLE_SHEETS);
+  const file = findFileByName(fileName, files);
+  if (file !== null) return SpreadsheetApp.openById(file.getId());
+
+  const headers = [
+    "Name",
+    "Vorname",
+    "Geburtsdatum",
+    "Email",
+    "Straße",
+    "Postleizahl",
+    "Stadt",
+    "Schiffre",
+  ];
+  const ss = SpreadsheetApp.create(fileName);
+  const sheet = ss.getActiveSheet();
+  sheet.getRange(1, 1, 1, headers.length).setValues([headers]);
+  return ss;
+}
+
+function createForm() {
+  const fileName = "Patienten Anlegen";
+  const files = DriveApp.getFilesByType(MimeType.GOOGLE_FORMS);
+  const file = findFileByName(fileName, files);
+  if (file !== null) return FormApp.openById(file.getId());
+
+  const form = FormApp.create(fileName);
+  form.setTitle(fileName);
+  form.setDescription("Hier kannst du einen Patienten Anlegen");
+
+  const emailValidation = FormApp.createTextValidation()
+    .requireTextIsEmail()
+    .build();
+
+  const numberValidation = FormApp.createTextValidation()
+    .requireTextMatchesPattern("^\\d{5}$")
+    .build();
+
+  form.addTextItem().setTitle("Name").setRequired(true);
+  form.addTextItem().setTitle("Vorname").setRequired(true);
+  form.addDateItem().setTitle("Geburtsdatum").setRequired(true);
+  const emailItem = form.addTextItem().setTitle("Email").setRequired(true);
+  emailItem.setValidation(emailValidation);
+
+  form.addTextItem().setTitle("Straße").setRequired(true);
+
+  const plzItem = form.addTextItem().setTitle("Postleizahl").setRequired(true);
+  plzItem.setValidation(numberValidation);
+
+  form.addTextItem().setTitle("Stadt").setRequired(true);
+  return form;
 }
