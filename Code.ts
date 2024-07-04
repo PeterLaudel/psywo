30e417955bddf8dbbdb67c282873521c1408e07f
Peter Laudel <38426989+PeterLaudel@users.noreply.github.com>
2024-07-04 19:12:01 +0200

    Implement on form submit

--- Code.ts
+++ Code.ts
@@ -3,6 +3,10 @@ function createEnvironment() {
   const form = createForm();
 
   form.setDestination(FormApp.DestinationType.SPREADSHEET, sheet.getId());
+  ScriptApp.newTrigger("onSubmitForm")
+    .forSpreadsheet(sheet)
+    .onFormSubmit()
+    .create();
 }
 
 function findFileByName(
@@ -24,20 +28,7 @@ function createSheet() {
   const file = findFileByName(fileName, files);
   if (file !== null) return SpreadsheetApp.openById(file.getId());
 
-  const headers = [
-    "Name",
-    "Vorname",
-    "Geburtsdatum",
-    "Email",
-    "Straße",
-    "Postleizahl",
-    "Stadt",
-    "Schiffre",
-  ];
-  const ss = SpreadsheetApp.create(fileName);
-  const sheet = ss.getActiveSheet();
-  sheet.getRange(1, 1, 1, headers.length).setValues([headers]);
-  return ss;
+  return SpreadsheetApp.create(fileName);
 }
 
 function createForm() {
@@ -71,4 +62,25 @@ function createForm() {
 
   form.addTextItem().setTitle("Stadt").setRequired(true);
   return form;
+}
+
+function onSubmitForm(e: GoogleAppsScript.Events.FormsOnFormSubmit) {
+  var sheet = SpreadsheetApp.getActiveSpreadsheet().getActiveSheet();
+  var lastRow = sheet.getLastRow();
+  var lastColumn = sheet.getLastColumn();
+
+  // Fügt eine neue Spalte nach der letzten Spalte hinzu, wenn sie noch nicht existiert
+  if (sheet.getRange(1, lastColumn).getValue() !== "Schiffre") {
+    sheet.insertColumnAfter(lastColumn);
+    sheet.getRange(1, lastColumn + 1).setValue("Schiffre");
+  }
+  const itemResponses = e.response.getItemResponses();
+  const lastName = itemResponses[0].getResponse().toString();
+  const birthDate = itemResponses[2].getResponse().toString();
+  const [month, day, year] = birthDate.split("/");
+  const shortYear = year.slice(-2); // Die letzten zwei Ziffern des Jahres
+  const cipher = `${month.padStart(2, "0")}${day.padStart(2, "0")}${shortYear}`;
+
+  // Setzt den Wert der Zelle in der neuen Spalte für die letzte Zeile
+  sheet.getRange(lastRow, lastColumn + 1).setValue(lastName[0] + cipher);
 }
