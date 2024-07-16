4af5f662a19a5356de7ff150c906a562229892c8
Peter Laudel <38426989+PeterLaudel@users.noreply.github.com>
2024-07-16 18:52:25 +0200

    Update administration sheet

--- documents/administration.ts
+++ documents/administration.ts
@@ -1,5 +1,7 @@
 export class Administration {
   private static spreadsheet_: GoogleAppsScript.Spreadsheet.Spreadsheet | null = null;
+  private static patientsSheet_: GoogleAppsScript.Spreadsheet.Sheet | null = null;
+  private static pricesSheet_: GoogleAppsScript.Spreadsheet.Sheet | null = null;
 
   public static get spreadsheet() {
     if (Administration.spreadsheet_ === null) {
@@ -9,5 +11,23 @@ export class Administration {
       Administration.spreadsheet_ = SpreadsheetApp.openById(id);
     }
     return Administration.spreadsheet_;
+  }
+
+  public static get patients() {
+    if (Administration.patientsSheet_ === null) {
+      Administration.patientsSheet_ = Administration.spreadsheet.getSheetByName(
+        "Patienten"
+      );
+    }
+    return Administration.patientsSheet_;
+  }
+
+  public static get prices() {
+    if (Administration.pricesSheet_ === null) {
+      Administration.pricesSheet_ = Administration.spreadsheet.getSheetByName(
+        "Preise"
+      );
+    }
+    return Administration.pricesSheet_;
   }
 }
